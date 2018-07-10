f45b60658af5bf26f168bb9b8e7dd67c8ba562b2
joshdreier <josh.dreier@ucsf.edu>
2018-07-09 17:02:04 -0700

    monday updates

--- app/scripts/faq.js
+++ app/scripts/faq.js
@@ -1,18 +1,74 @@
 var faq = [
   {
-    'q':'Is Color Factory in NYC the same as the one in San Francisco?',
-    'a':'Color Factory is starting from scratch with a whole new palette inspired by New York City. We are creating site-specific installations, collaborating with a new         roster of talented artists and creatives, and partnering with local non-profits and small food businesses we love.'
+    'q':'Is Color Factory NYC the same as San Francisco?',
+    'a':'Color Factory is starting from scratch with a whole new palette inspired by New York City. We are creating site-specific installations, collaborating with a new roster of talented artists and creatives, and partnering with local nonprofits and small food businesses we love.'
   },
   {
-    'q':'What do I need to bring?',
-    'a':'Just Yourself'
+    'q':'What should I bring?',
+    'a':'Along with your curiosity of color, you must bring your ticket and a photo ID.'
   },
   {
     'q':'Can I buy tickets at the door?',
-    'a':'No you can\'t'
+    'a':'Sadly, no. Tickets can only be purchased in advance through our website. Tickets purchased on third party websites or through scalpers are not permitted.'
   },
   {
     'q':'Can I give my ticket to someone else?',
-    'a':'Yes'
+    'a':'Tickets are non-refundable and non-transferrable. One person\'s ID in your party must match the name on one ticket (the rest of the names are interchangeable).'
+  },
+  {
+    'q':'What time do I need to arrive for my timed ticket?',
+    'a':'You will be welcomed into our lobby 15 minutes before your scheduled time. Please plan to arrive early. If you arrive 30 minutes after your purchased time slot, you will not receive access.'
+  },
+  {
+    'q':'Anything special I need to know about checking in?',
+    'a':'Our team will double check your ticket and ID at the door. Once inside, you\'ll register to access the photo booths throughout the exhibit and sign a required liability waiver.'
+  },
+  {
+    'q':'How long does it usually take to go through Color Factory?',
+    'a':'It varies for each guest, but visits usually last about an hour. But your experience extends beyond Color Factory with our free Color Map, which is offered at the end of your visit. Our map guides you through nearby neighborhoods to discover even more colorful surprises our team has created for you.'
+  },
+  {
+    'q':'Is the exhibit self-guided?',
+    'a':'Yes! And, you can take your time going through each room. However, we ask that you don\'t revisit any areas you\'ve already passed through. (One-way only!)'
+  },
+  {
+    'q':'Is Color Factory handicap accessible?',
+    'a':'Yes, we are ADA friendly and compliant.'
+  },
+  {
+    'q':'Is Color Factory appropriate for all ages?',
+    'a':'Color Factory is made with all audiences in mind. Children under the age of 18 must be accompanied by an adult at all times. There are some installations with features that are more about looking or listening than touching, so we ask that adults supervise accordingly. Wall text and our staff are there to guide you through to ensure you experience all the colors.'
+  },
+  {
+    'q':'Can I bring a stroller?',
+    'a':'Strollers are not permitted in the exhibit due to the sometimes intimate layout, however we provide stroller storage in our lobby.'
+  },
+  {
+    'q':'Can I bring food and drinks to Color Factory?',
+    'a':'We are sorry to say that outside food or drinks may not be brought into Color Factory. Special treats -- edible and otherwise -- are part of the experience and will be offered to you throughout certain installations.'
+  },
+  {
+    'q':'Are there bathrooms?',
+    'a':'There are bathrooms for guests in the lobby and halfway through the tour.'
+  },
+  {
+    'q':'What if I\'ve lost something in an exhibit?',
+    'a':'Please hold onto your hats (and cell phones)! Color Factory is not responsible for missing items. While we do have a lost and found, we cannot guarantee that your lost item will be found on site. Please contact support@colorfactory.co for more details.'
+  },
+  {
+    'q':'Are there any rules about taking photos?',
+    'a':'We want you to have fun and take in every moment. You are welcome to take photos, but please be considerate of others enjoying the exhibit. There are photo booths integrated throughout the exhibition, in hopes that it will free you to put your phone away, be in the photos with your friends and family, and appreciate the exhibits more fully. Tripods, accessive props, Commercial or Product shoots, press interviews, media shoots, all professional video equipment are prohibited without advance approval and accompaniment from a Color Factory representative. Contact press@colorfactory.co for requests.'
+  },
+  {
+    'q':'What does Color Factory do for the community?',
+    'a':'We are committed to collaborating and giving back to groups in the communities we create in. During our NYC run we\'ve collaborated and made donations to 826NYC, God\'s Love we Deliver, Coalition for the Homeless, Raices Texas, and LAND Gallery. A majority portion of all gift shop sales go to our partner nonprofits.'
+  },
+  {
+    'q':'Are there discounts for schools and students?',
+    'a':'We don\'t have student discounts but twice a month we bring in local schools from underserved neighborhoods. If you are a teacher or administrator at a Title 1 school, you can apply for a classroom visit, by emailing us at colorforthepeople@colorfactory.co  We will do our best to accommodate as space permits.'
+  },
+  {
+    'q':'Can I book a group tour or an event?',
+    'a':'Yes! Please contact nycgroups@colorfactory.co for pricing.'
   }
 ];
